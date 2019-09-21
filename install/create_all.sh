687fe21474a856e252d3b04a6528d276ee11addc
michaelhyatt <mhyatt8080 AT g m a i l DOT c  om>
2019-09-21 15:51:33 +1000

    incrasing timeouts

--- install/create_all.sh
+++ install/create_all.sh
@@ -6,8 +6,8 @@ kubectl create -f metricbeat/metricbeat-setup.yml
 kubectl create -f filebeat/filebeat-setup.yml
 
 # Wait for metricbeat and filebeat init
-kubectl wait --for=condition=complete job/filebeat-init --namespace=kube-system
-kubectl wait --for=condition=complete job/metricbeat-init --namespace=kube-system
+kubectl wait --for=condition=complete job/filebeat-init --namespace=kube-system --timeout=-1
+kubectl wait --for=condition=complete job/metricbeat-init --namespace=kube-system --timeout==-1
 
 kubectl create -f filebeat/filebeat.yml
 kubectl create -f metricbeat/metricbeat.yml
@@ -15,8 +15,8 @@ kubectl create -f nginx/nginx.yml
 kubectl create -f busybox/busybox.yml
 
 # wait to finish and run some curl commands
-kubectl wait --for=condition=available deployments/nginx
-kubectl wait --for=condition=ready pods/busybox
+kubectl wait --for=condition=available deployments/nginx --timeout=-1
+kubectl wait --for=condition=ready pods/busybox --timeout=-1
 
 kubectl exec busybox -- curl nginx-service
 kubectl exec busybox -- curl nginx-service/server-status
