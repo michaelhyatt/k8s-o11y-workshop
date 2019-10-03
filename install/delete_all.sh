f1457bf2e1da9bf7361a005999cb95414f665a7e
michaelhyatt <mhyatt8080 AT g m a i l DOT c  om>
2019-10-04 08:22:05 +1000

    fixed the delete

--- install/delete_all.sh
+++ install/delete_all.sh
@@ -1,10 +1,9 @@
 #!/bin/sh -xvf
 
-kubectl delete -f secret.yml
-kubectl delete -f kube-state-metrics
 kubectl delete -f metricbeat/metricbeat-setup.yml
 kubectl delete -f filebeat/filebeat-setup.yml
 kubectl delete -f filebeat/filebeat.yml
 kubectl delete -f metricbeat/metricbeat.yml
+kubectl delete -f mysql/mysql.yml
+kubectl delete -f petclinic/petclinic.yml
 kubectl delete -f nginx/nginx.yml
-kubectl delete -f busybox/busybox.yml
