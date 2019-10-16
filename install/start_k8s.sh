5089828e3ed14b79a6df0292f49cc2dd344e5a42
michaelhyatt <mhyatt8080 AT g m a i l DOT c  om>
2019-10-16 17:18:42 +1100

    typo woth redirect

--- install/start_k8s.sh
+++ install/start_k8s.sh
@@ -6,6 +6,6 @@ sudo mv /root/.kube /root/.minikube $HOME
 sudo chown -R $USER $HOME/.kube $HOME/.minikube
 sed -i 's|root|home/admin|g' $HOME/.kube/config
 
-kubectl create -f $HOME/k8s-o11y-workshop/kube-state-metrics >& /dev/null
+kubectl create -f $HOME/k8s-o11y-workshop/kube-state-metrics
 
 kubectl create -f $HOME/k8s-o11y-workshop/theia/theia.yml
