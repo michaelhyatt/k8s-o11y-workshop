e40c97dc630ca340aa4f670d43bc5b63840ca029
michaelhyatt <mhyatt8080 AT g m a i l DOT c  om>
2019-10-22 13:14:20 +1100

    fix permisssions

--- install/install-debian9.sh
+++ install/install-debian9.sh
@@ -31,6 +31,7 @@ then
    sudo usermod -aG sudo admin
    sudo gpasswd -a admin docker
    sudo cp -R $HOME/k8s-o11y-workshop /home/admin
-   sudo chown -R admin:admin /home/admin
+   sudo chown -R admin:admin /home/admin/k8s-o11y-workshop
+   sudo chmod -R ogu+rw /home/admin/k8s-o11y-workshop
    sudo su - admin
 fi
