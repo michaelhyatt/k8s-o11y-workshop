61f8c42b1244e51f62e70e331c4bd70103df2ed0
michaelhyatt <mhyatt8080 AT g m a i l DOT c  om>
2019-10-22 12:01:43 +1100

    confirm admin user

--- install/start_k8s.sh
+++ install/start_k8s.sh
@@ -1,5 +1,17 @@
 #!/bin/sh -xv
 
+ME=$(whoami)
+
+# Make sure it is running as admin user
+if [ $ME != "admin" ]
+then
+   echo not admin
+   sudo deluser admin
+   sudo adduser --home /home/admin --disabled-password --disabled-login --quiet --gecos "" admin
+   sudo usermod -aG sudo admin
+   sudo su - admin
+fi
+
 sudo minikube start --kubernetes-version=1.16.0 --vm-driver=none
 rm -rf $HOME/.kube/ $HOME/.minikube/
 sudo mv /root/.kube /root/.minikube $HOME
