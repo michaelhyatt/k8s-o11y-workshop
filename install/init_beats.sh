e007e4c8d9bee1249d62d74aa07d904444733fae
michaelhyatt <mhyatt8080 AT g m a i l DOT c  om>
2019-10-23 23:24:30 +1100

    Run setup in parallel and wait for all at once

--- install/init_beats.sh
+++ install/init_beats.sh
@@ -2,12 +2,11 @@
 
 # Run metricbeat setup and wait for it to complete
 kubectl create -f $HOME/k8s-o11y-workshop/metricbeat/metricbeat-setup.yml
-kubectl wait --for=condition=complete job/metricbeat-init --namespace=kube-system --timeout=30m
-
 # Run filebeat setup and wait for it to complete
 kubectl create -f $HOME/k8s-o11y-workshop/filebeat/filebeat-setup.yml
-kubectl wait --for=condition=complete job/filebeat-init --namespace=kube-system --timeout=30m
-
 # Run packetbeat setup and wait for it to complete
 kubectl create -f $HOME/k8s-o11y-workshop/packetbeat/packetbeat-setup.yml
+
+kubectl wait --for=condition=complete job/metricbeat-init --namespace=kube-system --timeout=30m
+kubectl wait --for=condition=complete job/filebeat-init --namespace=kube-system --timeout=30m
 kubectl wait --for=condition=complete job/packetbeat-init --namespace=kube-system --timeout=30m
